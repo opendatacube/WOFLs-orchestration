7c447e36b34702d73e7b4c0aca2ec122eefc48d0
David Gavin <david.gavin@ga.gov.au>
2019-05-20 12:24:12 +0200

    Added similar helper functions to retrieve metadata and upload files

--- Docker/no_tile_generate_fc.py
+++ Docker/no_tile_generate_fc.py
@@ -1,17 +1,35 @@
+#!/usr/bin/env python3
+
+
 import os
 import uuid
 import warnings
 from datetime import date
 
 import datacube
+import logging
 import yaml
 from datacube.helpers import write_geotiff
 from datacube.model import Measurement
 from fc.fractional_cover import fractional_cover
 from rio_cogeo.cogeo import cog_translate
 from rio_cogeo.profiles import cog_profiles
+from distutils.util import strtobool
+
+
+INPUT_S3_BUCKET = os.getenv('INPUT_S3_BUCKET',
+                            'dea-public-data')
+
+INPUT_FILE = os.getenv('INPUT_FILE',
+                       'file:///home/david/Downloads/sr/LC08_L1TP_074072_20160917_20170321_01_T1.yaml')
+OUTPUT_S3_BUCKET = os.getenv('OUTPUT_S3_BUCKET',
+                             'dea-public-data')
+
+#SOURCE_FILE = 'file:///home/david/Downloads/sr/LC08_L1TP_074072_20160917_20170321_01_T1.yaml'
 
 dc = datacube.Datacube(app='fc')
+
+MAKE_PUBLIC = bool(strtobool(os.getenv('MAKE_PUBLIC', 'false').lower()))
 
 bands_of_interest = ['green', 'red', 'nir', 'swir1', 'swir2']
 
@@ -29,6 +47,47 @@ fc_measurements = [
     Measurement(name='PV', units='percent', dtype='int16', nodata=-1),
     Measurement(name='NPV', units='percent', dtype='int16', nodata=-1),
     Measurement(name='UE', units='1', dtype='int16', nodata=-1)]
+
+
+def _get_s3_url(bucket_name, obj_key):
+    return 's3://{bucket_name}/{obj_key}'.format(
+        bucket_name=bucket_name, obj_key=obj_key)
+
+
+def _read_xml(dc, bucket, path):
+    logging.info('Loading xml: {}'.format(_get_s3_url(bucket, path)))
+
+    dataset_id = str(uuid.uuid5(uuid.NAMESPACE_URL, _get_s3_url(bucket, path)))
+
+    logging.info("Getting dataset: {}".format(dataset_id))
+    dataset = dc.index.datasets.get(dataset_id)
+
+    return dataset.metadata_doc
+
+
+def _upload(client, bucket, remote_path, local_file, makepublic=False, mimetype=None):
+    logging.info("Uploading file {} to bucket and path {}/{}".format(
+        local_file,
+        bucket,
+        remote_path
+    ))
+    data = open(local_file, 'rb')
+
+    extra_args = dict()
+
+    if makepublic:
+        extra_args['ACL'] = 'public-read'
+    if mimetype is not None:
+        extra_args['ContentType'] = mimetype
+
+    args = { 'ExtraArgs': extra_args }
+
+    client.meta.client.upload_fileobj(
+        Fileobj=data,
+        Bucket=bucket,
+        Key=remote_path,
+        **args
+    )
 
 
 def create_cog(source, output, bidx):
@@ -91,16 +150,18 @@ def write_fc_band(fc, key, filename):
     return output_filename
 
 
-def main():
-    source_filename = 'file:///home/david/Downloads/sr/LC08_L1TP_074072_20160917_20170321_01_T1.yaml'
-    sources = dc.index.datasets.get_datasets_for_location(source_filename, 'exact')
-    source = next(sources)
+def main(source_filename):
+    source = _read_xml(dc, INPUT_S3_BUCKET, source_filename)
+    #sources = dc.index.datasets.get_datasets_for_location(source_filename, 'exact')
+    #source = next(sources)
+
+    crs = 'EPSG:' + str(source.crs.epsg)
 
     query = {
-        'datasets': [source],
-        'crs': 'EPSG:' + str(source.crs.epsg),
+        'datasets': [source['id']],
+        'crs': crs,
         'resolution': (-30, 30),
-        'output_crs': source.crs,
+        'output_crs': crs,
         'product': source.type.name
     }
 
@@ -109,7 +170,10 @@ def main():
 
     fc = load_and_generate_fc(query)
     if fc:
+        # keep track of different FC Measurement files
         output_files = {}
+
+        # for each FC mesurement, output to TIFF and COG
         for measurement in fc_measurements:
             key = measurement['name']
             uncogged_output_file = write_fc_band(fc, key, filename)
@@ -122,6 +186,7 @@ def main():
             os.remove(uncogged_output_file)
             output_files[key] = target_filename
 
+        # Generate new YAML
         new_doc = source.metadata_doc_without_lineage()
         new_doc['id'] = str(uuid.uuid4())
         new_doc['creation_dt'] = date.today()
@@ -136,6 +201,33 @@ def main():
         with open(yaml_filename, 'w') as outfile:
             yaml.dump(new_doc, outfile, default_flow_style=False)
 
+        # Upload each band file
+        for fc_measurement in fc_measurements:
+            key = fc_measurement['name']
+            # Upload data to S3
+            _upload(
+                s3,
+                OUTPUT_S3_BUCKET,
+                s3_filepath + output_files[key],
+                local_file=output_files[key],
+                makepublic=MAKE_PUBLIC,
+                mimetype="image/tiff"
+            )
+
+        # Upload metadata to S3
+        _upload(
+            s3,
+            OUTPUT_S3_BUCKET,
+            s3_filepath + yaml_filename,
+            local_file=yaml_filename,
+            makepublic=MAKE_PUBLIC,
+            mimetype="application/x-yaml"
+        )
+
+        logging.info('Done!')
+    else:
+        logging.error('No FC Found')
+
 
 if __name__ == '__main__':
-    main()
+    main(INPUT_FILE)
